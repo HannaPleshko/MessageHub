1e4836352b1bdfebc8bc611898b9849b563e64f2
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-15 16:18:55 +0300

    add route for create user

---
+++ server/src/utils/typeorm/entities/User.ts
@@ -0,0 +1,26 @@
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column({ nullable: true })
+    email: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ select: false })
+    password: string;
+}
