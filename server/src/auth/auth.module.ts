fd3a75a7d85a43ae4dfe19061ecf530862c90b3d
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-17 09:23:40 +0300

    add auth route

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { User } from '../utils/typeorm'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../utils/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
