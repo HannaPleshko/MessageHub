47a580cbd9e6c4778d0a765d22ac543b191640f2
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-25 10:20:40 +0300

    form client

---
+++ client/src/Pages/Registrate/Registrate.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../../Components/NavForm/NavForm'
+
+const Registrate: React.FC = () => {
+    return (
+        <>
+            <Form nav={['name', 'surname', 'email', 'password']}/>
+        </>
+    )
+}
+
+export default Registrate
